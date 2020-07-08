379285e8a676d6269996c9480866fd3bfaa085fd
cazma <mcaz.dev@gmail.com>
2020-07-09 08:55:17 +0900

    doGetサンプル更新

--- src/examples/simpletrigger_doget.ts
+++ src/examples/simpletrigger_doget.ts
@@ -1,11 +1,5 @@
-import { Table as Users,       cols as usersCols }       from './db/tables/users'
-import { Table as Countries,   cols as countriesCols }   from './db/tables/countries'
-import { Table as Authorities, cols as authoritiesCols } from './db/tables/authorities'
+import { McTable } from '../modules/mc_table'
 
-import { growSeeds } from './db/grow';
-import { McRecord }  from './modules/mc_record'
-import { McTable }   from './modules/mc_table';
-import { isRecords } from './modules/utils/is';
 
 
 declare var global: any;
